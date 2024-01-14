71ca86d31775e8d982bc7a9e5e45377114b64e8b
Ntami Egbe <ntamiegbe00@gmail.com>
2024-01-14 10:20:42 +0100

    feat: added scaling effect on scroll

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,29 @@
+'use client'
+
 import Card from "@/components/Card"
 import { cardsData } from "@/data"
+import { motion, useScroll } from 'framer-motion'
+import { useEffect, useRef } from 'react'
 
 export default function Home() {
-  console.log(cardsData)
+  const container = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start start', 'end end']
+  })
+
+  // Used to track the progress of the scroll
+  
+  // useEffect(() => {
+  //   scrollYProgress.on("change", e => console.log(scrollYProgress))
+  // }, [])
+
   return (
-    <main className="mt-[50vh] mb-[100vh]">
+    <main ref={container} className="mt-[50vh] mb-[100vh]">
       {
         cardsData.map((data, index) => {
-         return <Card key={index} index={index} {...data} />
+          const targetScale = 1 - ((cardsData.length - index) * 0.05)
+          return <Card key={index} index={index} {...data} progress={scrollYProgress} range={[index * 0.25, 1]} targetScale={targetScale} />
         })
       }
     </main>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,23 +4,24 @@ import Image from 'next/image'
 import { motion, useScroll, useTransform } from 'framer-motion'
 import { useRef } from 'react'
 
-const Card = ({ index, title, description, imageSrc, backgroundColor }) => {
+const Card = ({ index, title, description, imageSrc, backgroundColor, progress, range, targetScale }) => {
     const container = useRef(null)
     const { scrollYProgress } = useScroll({
         target: container,
         offset: ['start end', 'start start']
     })
 
-    const scale = useTransform(scrollYProgress, [0, 1], [1.5, 1])
+    const imageScale = useTransform(scrollYProgress, [0, 1], [1.5, 1])
+    const cardScale = useTransform(progress, range, [1, targetScale])
 
     return (
         <div ref={container} className='h-screen flex items-center justify-center sticky top-0'>
-            <div style={{ backgroundColor: backgroundColor, top: `calc(-10% + ${index * 25}px)` }} className="h-[500px] w-[1000px] rounded-xl p-10 relative">
+            <motion.div style={{ scale: cardScale, backgroundColor: backgroundColor, top: `calc(-10% + ${index * 25}px)` }} className="h-[500px] w-[1000px] rounded-xl p-10 relative">
                 <h3 className="text-2xl text-center font-bold">{title}</h3>
-                <div className="flex justify-between items-center h-full">
+                <div className="md:flex justify-between items-center h-full">
                     <p>{description}</p>
-                    <div className="h-[80%] w-[60%] overflow-hidden relative m-3">
-                        <motion.div style={{ scale }} className="w-full h-full">
+                    <div className="h-[80%] w-[100%] md:w-[60%] overflow-hidden relative m-3">
+                        <motion.div style={{ scale: imageScale }} className="w-full h-full">
                             <Image
                                 fill
                                 src={`/${imageSrc}`}
@@ -30,7 +31,7 @@ const Card = ({ index, title, description, imageSrc, backgroundColor }) => {
                         </motion.div>
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     )
 }
