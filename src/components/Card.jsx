7a5eb393a5ec2dbea897974b1ffcdc7ee9f66fe1
Ntami Egbe <ntamiegbe00@gmail.com>
2024-01-14 09:41:30 +0100

    feat: created scaling effect for card image

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,33 @@
+'use client'
+
 import Image from 'next/image'
+import { motion, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
 
 const Card = ({ title, description, imageSrc, backgroundColor }) => {
-    console.log(title)
+    const container = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: container,
+        offset: ['start end', 'start start']
+    })
+
+    const scale = useTransform(scrollYProgress, [0, 1], [1.5, 1])
+
     return (
-        <div className='h-screen flex items-center justify-center sticky top-0'>
+        <div ref={container} className='h-screen flex items-center justify-center sticky top-0'>
             <div style={{ backgroundColor: backgroundColor }} className="h-[500px] w-[1000px] rounded-xl p-10">
                 <h3 className="text-2xl text-center font-bold">{title}</h3>
                 <div className="flex justify-between items-center h-full">
                     <p>{description}</p>
-                    <div className="h-[300px] w-[500px] relative m-3">
-                        <Image
-                            fill
-                            src={`/${imageSrc}`}
-                            alt='cards'
-                            className='rounded-2xl'
-                        />
+                    <div className="h-[80%] w-[60%] overflow-hidden relative m-3">
+                        <motion.div style={{ scale }} className="w-full h-full">
+                            <Image
+                                fill
+                                src={`/${imageSrc}`}
+                                alt='cards'
+                                className='rounded-2xl object-cover'
+                            />
+                        </motion.div>
                     </div>
                 </div>
             </div>
